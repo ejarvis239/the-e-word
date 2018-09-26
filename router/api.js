cce4b2814e008f76f1546b7822b2a470e4dcef4e
Emma <emmahodson23@gmail.com>
2018-09-26 11:28:49 +0100

    adding

--- router/api.js
+++ router/api.js
@@ -9,4 +9,6 @@ apiRouter.use('/articles', articleRouter);
 apiRouter.use('/comments', commentRouter);
 apiRouter.use('/users', userRouter);
 
+app.get('/', express.static('public'))
+
 module.exports = apiRouter;
