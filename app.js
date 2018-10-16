7a8fa97efd530eb6e28f46187a4021ff64a2f8e4
Emma <emmahodson23@gmail.com>
2018-10-16 11:13:36 +0100

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const apiRouter = require('./router/api');
 const { handle404s, handle400s, handle500s } = require('./errors')
+const cors = require('cors')
+app.use(cors())
+
 app.use(bodyParser.json(), express.static('public'))
 const DB_URL = process.env.DB_URL || require('./config/index.js')
 app.set("view engine", "ejs");
