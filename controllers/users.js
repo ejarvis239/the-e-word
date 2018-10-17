6777db0d382ceaac07a38b66c4967cd3dd25952c
Emma <emmahodson23@gmail.com>
2018-10-17 14:27:23 +0100

    updating the user

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const { User } = require('../models/index');
 
 const getUserByUsername = (req, res, next) => {
     const {username} = req.params
-    User.find({username: username}, '-_id -__v')
+    User.find({username: username}, '-__v')
     .then(([user]) => {
       if (user.length===0) throw {msg: 'user does not exist', status:404}
       res.status(200).send({user})
