29badcd82f8cd0868b2a9be4e2a53703e81e8aea
Emma <emmahodson23@gmail.com>
2018-10-17 15:37:35 +0100

    updating the user

--- controllers/comments.js
+++ controllers/comments.js
@@ -41,8 +41,11 @@ const addComment = (req, res, next) => {
     const newComment = req.body
     newComment.belongs_to = article_id
     Comment.create(newComment)
-    .populate('created_by', '-__v')
-    .populate('belongs_to', '-__v')
+      .then(comment => {
+        return Comment.findById(comment._id)
+          .populate('created_by', '-__v')
+          .populate('belongs_to', '-__v')
+      })
       .then(comment => {
         res.status(201).send({comment})
       })
